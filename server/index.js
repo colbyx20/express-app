bd27b64a8b39189d99fb2ccb1f9d2da962f62b95
Colby <colbyx20@gmail.com>
2022-11-18 14:31:50 -0500

    add more queries

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 const port = process.env.PORT || 5000; // look in environment variable for port 
+
 const app = express();
-// const{graphqlHTTP} = require('express-graphql');
-// const schema = require('./schema/schema');
 
 //this is our 1 endpoint
-// app.use('/graphql',graphqlHTTP({
-//     schema,
-//     graphiql: process.env.NODE_ENV === 'development',
-// }))
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === 'development',
+    })
+);
 
 app.listen(port,console.log(`server running on port ${port}`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const {users,professors} = require('../sampleData.js'); // bring int mock data 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLSchema} = require('graphql'); // create a type of each object
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLSchema, GraphQLList, buildClientSchema} = require('graphql'); // create a type of each object
 
 //user type 
 const UserType = new GraphQLObjectType({
@@ -15,15 +15,47 @@ const UserType = new GraphQLObjectType({
     })
 });
 
+const ProfessorType = new GraphQLObjectType({
+    name: "Professor",
+    fields: () => ({
+        id: {type:GraphQLID},
+        firstname: {type:GraphQLString},
+        lastname: {type: GraphQLString},
+        email:{type:GraphQLString},
+        privilege:{type:GraphQLInt},
+        fieldOfInterest:{type:GraphQLString},
+        appointments: {type:GraphQLList}
+    })
+});
+
 // create root query to grabe user by id
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
+        users:{
+            type: new GraphQLList(UserType),
+            resolve(parent,args){
+                return clients;
+            }
+        },
         user:{
             type:UserType,
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 return users.find(users => users.id === args.id);
+            }
+        },
+        professors:{
+            type: new GraphQLList(ProfessorType),
+            resolve(parent,args){
+                return clients;
+            }
+        },
+        professor:{
+            type:ProfessorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return users.find(professor => professor.id === args.id);
             }
         }
     }
