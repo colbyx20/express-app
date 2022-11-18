e1694399b011a4323740997fd575eddaec69d241
Colby <colbyx20@gmail.com>
2022-11-18 14:01:37 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000; // look in environment variable for port 
 const app = express();
-const{graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
+// const{graphqlHTTP} = require('express-graphql');
+// const schema = require('./schema/schema');
 
 //this is our 1 endpoint
 // app.use('/graphql',graphqlHTTP({
